aeca38f30a68eb3d59d06f18d60b4c8d30aed070
Valerie Loveland <vloveland@kbra.com>
2020-05-10 17:40:50 -0400

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
+      <form>
+        <label>
+          Poem Title:
+          <input type="text" name="title" />
+        </label>
+        {/* <input type="submit" value="submit" /> */}
+
+        {/* <label>
+          Status: */}
+        <option value="published">published</option>
+        <option value="out">out</option>
+
+        {/* </label> */}
+        <input type="add poem" value="add" />
+        {/* <label>
+          Date Written:
+          <input type="text" name="dateWritten" />
+        </label>
+        <input type="submit" value="submit" />
+        <label>
+          Magazine:
+          <input type="text" name="magazine" />
+        </label>
+        <input type="submit" value="submit" />
+        <label>
+          Dates Out:
+          <input type="text" name="title" />
+        </label>
+        <input type="submit" value="dates" />
+        <label>
+          Response:
+          <input type="text" name="response" />
+        </label>
+        <input type="submit" value="submit" /> */}
+      </form>
     </div>
   );
 }
